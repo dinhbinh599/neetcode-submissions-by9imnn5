17b58575877e2e22fae22639c763a2eb95a4f093
TomResker <47264781+dinhbinh599@users.noreply.github.com>
2026-05-03 22:40:50 +0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        var dict = new Dictionary<char, char>(){
+            { ']', '['},
+            {')', '('},
+            {'}', '{'}
+        };
+
+        foreach (var c in s){
+            if (dict.ContainsKey(c)){
+                if (stack.Count == 0 || stack.Pop() != dict[c]){
+                    return false;
+                }
+            }
+            else {
+                stack.Push(c);
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
