db232d86747b710600dbc59435e3330e67d40dda
TomResker <47264781+dinhbinh599@users.noreply.github.com>
2026-05-11 18:09:27 +0700

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var map = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++){
+            if (map.ContainsKey(target - nums[i])){
+                return [map[target-nums[i]], i];
+            }
+            map[nums[i]] = i;
+        }
+
+        return [0,0];
+    }
+}
